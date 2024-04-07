fa408b4c075808886517b271635496c99ebb09fd
Beata <beti11@protonmail.com>
2024-04-07 12:38:38 +0200

    Alignas, alignof.

--- shapes/main.cpp
+++ shapes/main.cpp
@@ -106,6 +106,8 @@ int main()
     findFirstShapeMatchingPredicate(shapes, perimeterBiggerThan20, "perimeter bigger than 20");
     findFirstShapeMatchingPredicate(shapes, areaLessThan10, "area less than 10");
 
+    std::cout << "alignof(Circle): " << alignof(Circle) << std::endl;
+
     return 0;
 }
 
